9f2031111d373905e752e689af643b420957d30a
Alex Shi <kevinsshi@hotmail.com>
2020-07-24 14:36:34 -0400

    Updates

---
+++ precache-manifest.d7d331d342435e6da0d9e2106b369c90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088e87cf863e0a5bbd3b6c75ce5b7f99",
+    "url": "/spotify-web/index.html"
+  },
+  {
+    "revision": "a4660f4cd92be348218e",
+    "url": "/spotify-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fa025538f288eb196a0d",
+    "url": "/spotify-web/static/js/2.a6f6046f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spotify-web/static/js/2.a6f6046f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4660f4cd92be348218e",
+    "url": "/spotify-web/static/js/main.8c8e2b22.chunk.js"
+  },
+  {
+    "revision": "2e2a1add812ed5042167",
+    "url": "/spotify-web/static/js/runtime-main.209230a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spotify-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
